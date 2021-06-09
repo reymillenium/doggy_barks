ce97d23b122f3f93c00661da467486845af976e2
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-06-09 02:46:29 -0400

    Added the has_many relation to the User model with the Dog class. Added the belongs_to relation to the Dog model with the USer class

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,4 +3,9 @@ class Dog < ApplicationRecord
 
   # Defines the amount of rows per page using the will_paginate gem:
   self.per_page = 5
+
+  # Relations:
+  # Assumption # 1: As the challenge doesn't gives much information => any User object is a potential owner, without any extra condition
+  # Assumption # 2: The already existing Dog objects on the DB doesn't have any owner & a Dog object can exist without an owner (therefore the belongs_to relation is optional)
+  belongs_to :user, optional: true, foreign_key: :user_id, class_name: User.class_name, inverse_of: :dogs
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Relations
+  # Assumption: A Dog object can exist without an owner (User object). Therefore there is not a destroy dependent relationship
+  has_many :dogs, inverse_of: :user, foreign_key: :user_id, inverse_of: :user
 end
