69cdb551f4367275e2f7dba81c40e9c9c51417a1
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-06-09 04:14:37 -0400

    Fixed minor issue on relation between Dog model and User model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -7,6 +7,6 @@ class Dog < ApplicationRecord
   # Relations:
   # Assumption # 1: As the challenge doesn't gives much information => any User object is a potential owner, without any extra condition
   # Assumption # 2: The already existing Dog objects on the DB doesn't have any owner & a Dog object can exist without an owner (therefore the belongs_to relation is optional)
-  belongs_to :user, optional: true, foreign_key: :user_id, class_name: User.class_name, inverse_of: :dogs
+  belongs_to :user, optional: true, foreign_key: :user_id, class_name: User.class.name, inverse_of: :dogs
   has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   # Relations
   # Assumption: A Dog object can exist without an owner (User object). Therefore there is not a destroy dependent relationship
-  has_many :dogs, inverse_of: :user, foreign_key: :user_id, inverse_of: :user
+  has_many :dogs, foreign_key: :user_id, inverse_of: :user
   has_many :likes
 end
