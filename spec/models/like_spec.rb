50b7466c1ac1572c91b844d90c2ab928ccb67de2
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-06-11 02:19:48 -0400

    Added some rspec tests to cover the scopes: 'by_user' & 'by_dog' on the Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -22,4 +22,55 @@ RSpec.describe Like, type: :model do
 
     it { is_expected.to belong_to(:dog).class_name('Dog') }
   end
+
+  # Scopes:
+  describe '.by_user' do
+    let(:current_dogs) { 2.times.map { create :dog } }
+    let(:current_user) { create :user }
+    let(:other_user) { create :user }
+
+    let!(:likes_created_by_current_user_and_one_dog) { 2.times.map { |index| create :like, user: current_user, dog: current_dogs[index] } }
+    let!(:likes_created_by_other_user_and_one_dog) { 2.times.map { |index| create :like, user: other_user, dog: current_dogs[index] } }
+
+    it 'should be defined' do
+      expect(described_class).to respond_to :by_user
+    end
+
+    it 'should return an active record relationship' do
+      expect(described_class.by_user(current_user)).to be_a ActiveRecord::Relation
+    end
+
+    it 'should return all the likes created by the current user' do
+      expect(described_class.by_user(current_user.id)).to match_array likes_created_by_current_user_and_one_dog
+    end
+
+    it 'should not return any like when the current user is nil' do
+      expect(described_class.by_user(nil)).to be_empty
+    end
+  end
+
+  describe '.by_dog' do
+    let(:current_users) { 2.times.map { create :user } }
+    let(:current_dog) { create :dog }
+    let(:other_dog) { create :dog }
+
+    let!(:likes_created_by_current_dog_and_one_user) { 2.times.map { |index| create :like, dog: current_dog, user: current_users[index] } }
+    let!(:likes_created_by_other_user_and_one_user) { 2.times.map { |index| create :like, dog: other_dog, user: current_users[index] } }
+
+    it 'should be defined' do
+      expect(described_class).to respond_to :by_dog
+    end
+
+    it 'should return an active record relationship' do
+      expect(described_class.by_dog(current_dog)).to be_a ActiveRecord::Relation
+    end
+
+    it 'should return all the likes created by the current dog' do
+      expect(described_class.by_dog(current_dog.id)).to match_array likes_created_by_current_dog_and_one_user
+    end
+
+    it 'should not return any like when the current dog is nil' do
+      expect(described_class.by_dog(nil)).to be_empty
+    end
+  end
 end
