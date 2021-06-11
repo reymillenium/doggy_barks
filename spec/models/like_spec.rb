1e1118e99f334c4e678951fe1e0e47b5d101c63b
Reinier Garcia Ramos <reymillenium@gmail.com>
2021-06-11 02:01:56 -0400

    Added a few rspec tests to cover the Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:described_object) { build :like }
+
+  it "has a valid factory" do
+    expect(FactoryBot.build(:like)).to be_valid
+  end
+
+  describe '#user' do
+    it 'should be defined' do
+      expect(described_object).to respond_to :user
+    end
+
+    it { is_expected.to belong_to(:user).class_name('User') }
+  end
+
+  describe '#dog' do
+    it 'should be defined' do
+      expect(described_object).to respond_to :user
+    end
+
+    it { is_expected.to belong_to(:dog).class_name('Dog') }
+  end
 end
